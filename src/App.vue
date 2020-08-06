3a756411cecd605fcea846203da40b3409c87b44
francisquinn <francisquinnn@gmail.com>
2020-08-06 17:36:17 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="App">
+    <Header></Header>
+    <v-main class="mx-5 my-2">
+      <router-view></router-view>
+    </v-main>
+    <Footer></Footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from '@/components/Header'
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
-}
+  components: {
+    Header
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <v-app-bar app fixed color="grey" flat>
+        <span>(Logo + Name)</span>
+        <v-spacer></v-spacer>
+      <div class="pa-2" v-for="page in pages" :key="page.text">
+        <router-link id="link" :to="`${page.route}`">
+          <v-btn id="button" text>{{page.text}}</v-btn>
+        </router-link>
+      </div>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    pages: [
+      { text: "Home", route: "/" },
+      { text: "About", route: "/about" },
+      {text: "Contact", route: "/contact"}
+    ]
+  })
+};
+</script>
+
+<style>
+</style>
